12b4b741dad3c1b65753f00f72ce7017142a1d1d
Ahsan <ahsan.qureshi8@gmail.com>
2024-03-08 19:00:41 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ahsan Qureshi on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
