fcac3548af56974bd0736971a32dba35f8b52862
Ahsan <ahsan.qureshi8@gmail.com>
2024-03-16 15:20:30 -0400

    red total if 0% tip

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -51,6 +51,7 @@ struct ContentView: View {
                 
                 Section("Total") {
                     Text(total, format: .currency(code: currencyCode))
+                        .foregroundStyle(tipPercentage == 0 ? .red : .primary)
                 }
                 
                 Section("Amount per person") {
